983cf9c89989f2a68e49165c6767e9abf6629595
Alex Priem <alexpriem@gmail.com>
2013-12-08 15:46:31 +0100

    recordinfo: python side.

--- build.py
+++ build.py
@@ -1,4 +1,4 @@
-import pyodbc
+import pyodbc,sys
 import argparse
 from jinja2 import Environment, Template
 
@@ -13,13 +13,64 @@ class calendar:
     def build_data(self,args):
         lines=open("data/orig/knmi_hvh_2009.txt","r").readlines()
 
+        recordinfo=args['recordinfo']
+        if recordinfo is None:
+            raise RuntimeError('recordinfo needed, add -record to cmdline')
+        cols=recordinfo.split(',')
+        cols=[col.strip() for col in cols]
+        key=None
+        c=0
+        if 'key' in cols:
+            keyindex=cols.index('key')
+            c+=1
+        keyidindex=None
+        if 'keyid' in cols:
+            keyidindex=cols.index('keyid')
+            c+=1
+        if not('date' in cols):
+            raise  RuntimeError('date-col needed in record')
+        if not('hour' in cols):
+            raise  RuntimeError('date-col needed in record')
+        dateindex=cols.index('date')
+        hourindex=cols.index('hour')
+        datacolnames=[];
+        for col in cols:
+            if col not in ['key','keyid','date','hour','dummy']:
+                datacolnames.append(col)
+        dataindex=[]
+        for datacolname in datacolnames:
+            dataindex.append(cols.index(datacolname))
+
+        js='var meta=[';
+        if keyindex is not None:
+            js+='"key",'
+        if keyidindex is not None:
+            js+='"keyid",'
+        js+='"date","hour",'
+        js+='"'+'","'.join(datacolnames)
+        js+='"];\n'
+        
+        
+            
+        
+
         firstline=True
-        js='var data=[\n'
+        js+='var data=[\n'
         for line in lines:
-            line=line.strip();
+            line=line.strip();            
             if line[0]=='#':
                 continue
-            js+='['+line+'],\n'
+            datacols=line.split(',')
+            datacols=[datacol.strip() for datacol in datacols]
+            js+='['
+            if keyindex is not None:
+                js+=datacols[keyindex]            
+            js+=','+datacols[dateindex]
+            js+=','+datacols[hourindex]            
+            js+=','+','.join([datacols[d] for d in dataindex])
+            js+='],\n'
+            
+            
         js=js[:-2]+'];\n';
         g=open("js/data.js","w");
         g.write(js)
@@ -40,8 +91,9 @@ parser = argparse.ArgumentParser(description='generate calendar from repeating d
 #parser.add_argument('-u', dest='username',  help='username', required=False)
 #parser.add_argument('-p', dest='password',  help='password', required=False)
 
+parser.add_argument('-c', '--csv', dest='csv',  help='csv input file name', required=True)
 parser.add_argument('-html', dest='htmlfile',  help='name of html outputfile', required=False, default=True, action='store_true')
-parser.add_argument('-records', dest='records',  help='record description', required=False, default=True, action='store_true')
+parser.add_argument('-record', dest='recordinfo',  help='record description: key,keyid,date,hour,datalabel,dummy', required=True)
 
 args=vars(parser.parse_args())
 

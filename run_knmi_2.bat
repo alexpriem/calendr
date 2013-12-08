ca148d9a095d88d666904f769d181bb20f982586
Alex Priem <alexpriem@gmail.com>
2013-12-08 17:19:12 +0100

    more testcases

---
+++ run_knmi_2.bat
@@ -0,0 +1,1 @@
+python build.py -c data\orig\KNMI_hvh_2009.txt -html -record "key,date,hour,Temperatuur,dummy,td,Luchtdruk" -keyfile data\orig\station_ids.txt
---
+++ run_knmi_3.bat
@@ -0,0 +1,1 @@
+python build.py -c data\orig\KNMI_3_hourly.txt -html -record "key,date,hour,Temperatuur,dummy,td,Luchtdruk" -keyfile data\orig\station_ids3.txt
